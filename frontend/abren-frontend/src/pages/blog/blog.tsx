a91e5716f6c0c9179b3b4f107af093d79b1d55ec
Esrael Zerihun <144590580+ezraelz@users.noreply.github.com>
2025-05-10 22:05:45 -0700

    abren repo

---
+++ frontend/abren-frontend/src/pages/blog/blog.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Blog = () => {
+    
+  return (
+    <div className='blog'>
+      <div className="blog-hero">
+        <h1>Blog</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Blog
