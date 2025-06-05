307f083ec34b15a99249668d91eaaff158ed188c
Esrael Zerihun <144590580+ezraelz@users.noreply.github.com>
2025-06-04 23:41:21 -0700

    Initial commit - clean slate

--- frontend/abren-frontend/src/utils/axios.ts
+++
@@ -1,9 +0,0 @@
-// axios.ts or main axios config file
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:8000', // match your backend
-  withCredentials: true,
-});
-
-export default instance;
